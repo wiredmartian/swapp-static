54c801a1d25e516fb808257097f98ee5649fdd1b
wiredmartian <meggatym@yahoo.com>
2020-05-11 23:09:31 +0200

    ignore encoding errors explicitly

--- main.go
+++ main.go
@@ -18,9 +18,8 @@ func main() {
 	router.HandleFunc("/api/ping", pingAPI).Methods("GET")
 	router.HandleFunc("/api/upload", uploadFileHandler).Methods("POST")
 	router.PathPrefix("/static").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("./static/"))))
-
-	log.Fatal(http.ListenAndServe(":8001", router))
 	fmt.Println("Server running on PORT 8001")
+	log.Fatal(http.ListenAndServe(":8001", router))
 }
 
 /** Models */
@@ -32,19 +31,19 @@ type FileUploadRes struct {
 /** Handlers */
 func pingAPI(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
-	json.NewEncoder(w).Encode("Hello from Go api!")
+	_ = json.NewEncoder(w).Encode("Hello from Go api!")
 }
 func getFile(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./static/11290243.jpeg")
 }
 func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(5 * 1024 * 1024)
+	_ = r.ParseMultipartForm(5 * 1024 * 1024)
 	file, header, err := r.FormFile("image")
 	if err != nil {
 		fmt.Println(err)
 		w.Header().Set("content-type", "application/json")
 		w.WriteHeader(400)
-		json.NewEncoder(w).Encode(err)
+		_ = json.NewEncoder(w).Encode(err)
 		return
 	}
 	defer file.Close()
@@ -52,12 +51,12 @@ func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("%v\n", header.Size)
 	fmt.Printf("%v\n", header.Header)
 
-	tempFile, error := ioutil.TempFile("static", "upload-*.png")
-	if error != nil {
-		fmt.Println(error)
+	tempFile, _error := ioutil.TempFile("static", "upload-*.png")
+	if _error != nil {
+		fmt.Println(_error)
 		w.Header().Set("content-type", "application/json")
 		w.WriteHeader(400)
-		json.NewEncoder(w).Encode(error)
+		_ = json.NewEncoder(w).Encode(_error)
 		return
 	}
 	defer tempFile.Close()
@@ -67,7 +66,7 @@ func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 		w.Header().Set("content-type", "application/json")
 		w.WriteHeader(400)
-		json.NewEncoder(w).Encode(err)
+		_ = json.NewEncoder(w).Encode(err)
 		return
 	}
 	bytesW, _err := tempFile.Write(fileBytes)
@@ -75,13 +74,13 @@ func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 		w.Header().Set("content-type", "application/json")
 		w.WriteHeader(400)
-		json.NewEncoder(w).Encode(_err)
+		_ = json.NewEncoder(w).Encode(_err)
 		return
 	}
 	fmt.Println(bytesW)
 	res := FileUploadRes{Message: "Successfully uploaded file", FileUrl: tempFile.Name()}
 	w.Header().Set("content-type", "application/json")
-	json.NewEncoder(w).Encode(res)
+	_ = json.NewEncoder(w).Encode(res)
 }
 
 /** Middleware */
